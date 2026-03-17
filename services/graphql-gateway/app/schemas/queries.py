c0a396b144bff3de0bc119ead846e67d90b12380
Saitharunmotipeta <motipetasaitharun@gmail.com>
2026-03-17 21:59:35 +0530

    pagination , recent , severity filters

--- services/graphql-gateway/app/schemas/queries.py
+++ services/graphql-gateway/app/schemas/queries.py
@@ -1,7 +1,7 @@
 import strawberry
 from typing import List
 
-from app.services.insight_service import get_insights_by_account
+from app.services.insight_service import get_insights_by_account, get_insights_paginated, get_insights_by_severity, get_recent_insights
 from app.schemas.types import InsightType
 
 
@@ -24,4 +24,61 @@ class Query:
                 generated_at=i.generated_at,
             )
             for i in results
+        ]
+    
+    @strawberry.field
+    def recent_insights(self, limit: int = 10) -> List[InsightType]:
+        insights = get_recent_insights(limit)
+
+        return [
+            InsightType(
+                id=str(i.id),
+                account_id=str(i.account_id),
+                service=i.service,
+                severity=i.severity,
+                message=i.message,
+                recommendation=i.recommendation,
+                generated_at=i.generated_at,
+            )
+            for i in insights
+        ]
+    
+    @strawberry.field
+    def insights_paginated(
+        self, account_id: str, limit: int = 10, offset: int = 0
+    ) -> List[InsightType]:
+
+        insights = get_insights_paginated(account_id, limit, offset)
+
+        return [
+            InsightType(
+                id=str(i.id),
+                account_id=str(i.account_id),
+                service=i.service,
+                severity=i.severity,
+                message=i.message,
+                recommendation=i.recommendation,
+                generated_at=i.generated_at,
+            )
+            for i in insights
+        ]
+    
+    @strawberry.field
+    def insights_by_severity(
+        self, severity: str
+    ) -> List[InsightType]:
+
+        insights = get_insights_by_severity(severity)
+
+        return [
+            InsightType(
+                id=str(i.id),
+                account_id=str(i.account_id),
+                service=i.service,
+                severity=i.severity,
+                message=i.message,
+                recommendation=i.recommendation,
+                generated_at=i.generated_at,
+            )
+            for i in insights
         ]
--- services/graphql-gateway/app/services/insight_service.py
+++ services/graphql-gateway/app/services/insight_service.py
@@ -13,3 +13,44 @@ def get_insights_by_account(account_id: str):
 
     db.close()
     return results
+
+def get_recent_insights(limit: int):
+    db = SessionLocal()
+
+    results = (
+        db.query(Insight)
+        .order_by(Insight.generated_at.desc())
+        .limit(limit)
+        .all()
+    )
+
+    db.close()
+    return results
+
+def get_insights_paginated(account_id: str, limit: int, offset: int):
+    db = SessionLocal()
+
+    results = (
+        db.query(Insight)
+        .filter(Insight.account_id == UUID(account_id))
+        .order_by(Insight.generated_at.desc())
+        .limit(limit)
+        .offset(offset)
+        .all()
+    )
+
+    db.close()
+    return results
+
+def get_insights_by_severity(severity: str):
+    db = SessionLocal()
+
+    results = (
+        db.query(Insight)
+        .filter(Insight.severity == severity)
+        .order_by(Insight.generated_at.desc())
+        .all()
+    )
+
+    db.close()
+    return results
